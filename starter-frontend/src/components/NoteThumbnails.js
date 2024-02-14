83119c1058d433f1537499f72e0baa0cc400ce0a
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2024-02-13 16:28:45 -0800

    Updated Notes page
    Changed note thumbnail text height and added link to note page

--- starter-frontend/src/components/NoteThumbnails.js
+++ starter-frontend/src/components/NoteThumbnails.js
@@ -1,21 +1,27 @@
-function NoteThumbnail({title, text}) {
+import { Link } from 'react-router-dom';
+
+function NoteThumbnail({title, text, id}) {
     return (
-        <div className="thumbnail">
-            <div className="thumbnail-body">
-                <p className="thumbnail-text">{text}</p>
+        // Later make into
+        // <Link to={`../note/${id}`} className="link">
+        <Link to={`../note`} className="link">
+            <div className="thumbnail">
+                <div className="thumbnail-body">
+                    <p className="thumbnail-text">{text}</p>
+                </div>
+                <div className="thumbnail-label">
+                    <p className="thumbnail-title">{title}</p>
+                </div>
             </div>
-            <div className="thumbnail-label">
-                <p className="thumbnail-title">{title}</p>
-            </div>
-        </div>
+        </Link>
     );
 }
 
 export default function NoteThumbnails() {
     return (
         <>
-            <NoteThumbnail title="Testing" text="blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah " />
-            <NoteThumbnail title="Testing 2" text="cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus " />
+            <NoteThumbnail title="Testing" text="blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah " id="123" />
+            <NoteThumbnail title="Testing 2" text="cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus cactus " id="cactus" />
         </>
     );
 }
