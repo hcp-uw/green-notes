a44e035c636f22e2c764370e5cbb152b98e501fc
Zachary H <zhanczyc@cs.washington.edu>
2024-02-23 17:42:40 -0800

    changed folder page color and comments

--- starter-frontend/src/pages/folder.js
+++ starter-frontend/src/pages/folder.js
@@ -1,8 +1,10 @@
 import { Form } from "react-router-dom";
 
+// In the future, instead of having a separate folder page it will just
+// edit what content is being shown on the notes page
 export default function Folder() {
     return (
-        <div className="page gray-background">
+        <div className="page green-background">
             <p>Placeholder Folder</p>
         </div>
     );
