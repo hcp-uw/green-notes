1585bda03e6bf51cdb17eeb889f3aa4b01a5ae51
Zachary H <zth12301@gmail.com>
2024-04-25 23:24:39 -0700

    test commit

--- starter-frontend/src/contexts/AuthContext.js
+++ starter-frontend/src/contexts/AuthContext.js
@@ -21,6 +21,8 @@ export function useAuth() {
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState();
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
   
     function register(email, password) {
       return createUserWithEmailAndPassword(auth, email, password);
@@ -43,6 +45,8 @@ export function AuthProvider({ children }) {
       currentUser,
       login,
       register,
+      error,
+      setError,
     };
   
     return (
