333906426e7dc1f1de442b03de91d34c809ed4d6
Kemin Li <likemin05@gmail.com>
2024-05-18 15:42:01 -0700

    uhm
    guys what the fduafksagkj

--- starter-frontend/src/pages/profile.tsx
+++ starter-frontend/src/pages/profile.tsx
@@ -1,8 +1,17 @@
 //@ts-nocheck
+import profile from '../assets/profile-button.png';
+
+
 export default function Profile() {
     return (
         <div className="page green-background">
-            <p>Placeholder Profile</p>
+            <div className="background">
+                <p className="profile-icon"><img id="profile-icon" src={profile}></img></p>
+            </div>  
+
+            <div>
+                
+            </div>
         </div>
         
     );
