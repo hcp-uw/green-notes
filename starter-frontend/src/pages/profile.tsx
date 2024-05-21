c17f1e1e6954f607fac9a196d1adc74f78553051
Kemin Li <likemin05@gmail.com>
2024-05-20 22:11:11 -0700

    oh uippead
    ohgawdlordpleasehelp

--- starter-frontend/src/pages/profile.tsx
+++ starter-frontend/src/pages/profile.tsx
@@ -1,18 +1,42 @@
 //@ts-nocheck
 import profile from '../assets/profile-button.png';
-
+import "./profile.css";
 
 export default function Profile() {
     return (
-        <div className="page green-background">
-            <div className="background">
-                <p className="profile-icon"><img id="profile-icon" src={profile}></img></p>
-            </div>  
-
-            <div>
-                
+        <div className="profile-page">
+            <div className="white-box">
+                <User />
             </div>
-        </div>
-        
+        </div>        
     );
 }
+
+function User() {
+    return (
+        <div>
+            <Icon />
+            <Bio />
+        </div>
+    );
+}
+
+function Icon() {
+    return (
+        <div className="profile-icon"><img className="profile-icon" src={profile}></img></div>
+    );
+}
+
+function Bio() {
+    return (
+        <div className="bio-box">
+            <div className="bio-text">
+                <p>
+                    Hello my name is john doe.
+                </p>
+            </div>
+        </div> 
+    );
+}
+
+
