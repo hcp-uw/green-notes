48fa30d2db3a4c915d9c1e3957c16fc41d2059b8
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2024-06-15 13:11:28 -0700

    Converted ErrorPage
    Didn't change to CSE 331 style code

--- starter-frontend/src/pages/error-page.tsx
+++ starter-frontend/src/pages/error-page.tsx
@@ -1,18 +1,30 @@
-//@ts-nocheck
-import { useRouteError } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { useRouteError, isRouteErrorResponse, Link } from "react-router-dom";
 
-export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
-
+export default function ErrorPage(): JSX.Element {
     return (
         <div id="error-page">
             <p>Placeholder error page</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+            <Error />
             <Link to={`/`}>Click here to go home</Link>
         </div>
     );
 }
+
+
+function Error(): JSX.Element {
+    const error : unknown = useRouteError();
+    console.error(error);
+
+    if (isRouteErrorResponse(error)) {
+        return (
+            <div>
+                <p>{error.status}</p>
+                <p>{error.statusText}</p>
+                {error.data?.message && <p>{error.data.message}</p>}
+            </div>
+        );
+    } else {
+        return <></>;
+    }
+}
+    
