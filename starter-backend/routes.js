baced2d026e0f1947df4affe61e933a4334b247b
Zachary H <zth12301@gmail.com>
2024-07-01 18:26:40 -0700

    minor changes

--- starter-backend/routes.js
+++ starter-backend/routes.js
@@ -18,7 +18,13 @@ export async function test(req, res)  {
     res.send("added hopefully");
 }
 
-// Needs to be given route of doc in "userEmail/notes/folder/subfolder/name"
+
+// Needs to be given route of doc in "Users/{userEmail}/Notes/{folderID}/content/{name}"
+// See db for how data is structured to ensure you are giving a proper route
+// Curly brackets mean that the text is just an example. All text outside of
+// curly brackets is necessary for every call (a "/content" is required after
+// every subfolder in "Notes" due to how firebase works).
+// You may have any number of subfolders in the route, but each must be followed by a "/content"
 export async function getNote(req, res) {
 
     const route = req.query.route;
@@ -29,6 +35,8 @@ export async function getNote(req, res) {
     }
 
     // Also add in a way to check that the given user is properly logged in
+    // Maybe do this through firestore db permissions, but it might not have
+    // that power
 
     const noteRef = db.doc(route);
     const doc = await noteRef.get();
@@ -39,5 +47,30 @@ export async function getNote(req, res) {
         res.send({data: doc.data()});
         return;
     }
+};
 
+
+// Gets all folders and docs inside given route to a collection
+// See route parameterse in get doc function above
+export async function getFolderContents(req, res) {
+
+    const route = req.query.route;
+
+    if (typeof route !== "string") {
+        res.status(400).send('missing or invalid "route" parameter');
+        return;
+    }
+
+    const collectionRef = db.collection(route);
+    const snapshot = await collectionRef.get();
+
+    if (!snapshot.exists) {
+        res.send("no folder found :(");
+        return;
+    } else {
+        snapshot.forEach(item => {
+
+        })
+        return;
+    }
 }
--- starter-frontend/src/pages/notes/notes.tsx
+++ starter-frontend/src/pages/notes/notes.tsx
@@ -26,9 +26,10 @@ export default function Notes(): JSX.Element {
     const [isAdvanced, setIsAdvanced] = useState<boolean>(false);
 
     // Keeps track of folder route user is in
-    let currRoute: route = nil;
+    const [currRoute, setCurrRoute] = useState<route>(nil);
+
     // Starts empty since we haven't checked what user id they are yet.
-    const currLocation: string = "";
+    const [currLocation, setcurrLocation] = useState<string>("");
 
     // Gets user object from auth in order to get user info
     const user = auth.currentUser;
@@ -36,10 +37,23 @@ export default function Notes(): JSX.Element {
         if (user.email === null) { // if user doesn't have email (shouldn't be possible)
             return<p>error</p>
         }
-        currRoute = cons(user.email, currRoute)
+        setCurrRoute(cons(user.email, currRoute));
     } else { // If user isn't logged in (should be impossible on this page)
         return<p>error</p>
     }
+
+    /* TODO:
+        Make a way to store server side data for contents in folders
+        so we don't need to make a new call every single time someone
+        clicks on a folder, only when they click on the folder for 
+        the first time in the window.
+        
+        Perhaps use a map with strings as the keys and jsx.element arrays
+        as the locations and use "current location" as a way find it.
+        "current location" would be in "home/folder/folder" format to ensure
+        that it still works even if there are several folders with the same name
+        in different places. There cant be 2 folders of the same name in the
+        same location*/
 
 
     if (currLocation === "") { // If user isn't in a folder ** folder functionality hasn't been implemented yet
@@ -80,3 +94,18 @@ const PreviousFolder = ({name}: PreviousFolderProps): JSX.Element => {
         </div>
     )
 };
+
+// Method to be called to grab folder contents from server
+const getFolderContents = (name: string): JSX.Element[] => {
+    const data: JSX.Element[] = [];
+    // make call to server to grab folder contents.
+    // What we need: folders (collections) and doc names and ids all
+    //              within given folder name.
+    // We give: folder id and full route
+    return [];
+};
+
+const onFolderClick = (name: string): void => {
+    // call getFolderContents and update the current states to
+    // show the proper things
+}
