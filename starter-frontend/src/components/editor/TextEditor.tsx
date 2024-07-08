22528223dcf2b73ed1f520fd150482b9150006dd
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2024-07-08 14:33:08 -0700

    Made editor height change according to window height

--- starter-frontend/src/components/editor/TextEditor.tsx
+++ starter-frontend/src/components/editor/TextEditor.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import tinymce from 'tinymce';
 
 export default function TextEditor() {
     const editorRef = useRef(null);
@@ -7,8 +8,10 @@ export default function TextEditor() {
       <div id="editor-area">
         <Editor 
           tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+          id='editor'
           init={{
-            height: 470,
+            height: "calc(100vh - 105px)",
+            resize: false, 
             menubar: false,
             plugins: [
               'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
@@ -24,4 +27,33 @@ export default function TextEditor() {
         />
       </div>
     );
+}
+
+
+function editorHeight(): number {
+  const area: HTMLElement | null = document.getElementById('editor-area');
+  if (area === null) {
+    throw new Error("editor-area is null");
+  }
+
+  const areaHeight: number = area.offsetHeight;
+  const editorHeight: number = areaHeight - 75;
+  return editorHeight;
+}
+
+function resizeHeight(): void {
+  const editor: HTMLElement | null = document.getElementById('editor');
+  if (editor === null) {
+    throw new Error("tox-tinymce is null");
+  }
+
+  editor.style.height = `150px`;
+}
+
+document.onload = function () {
+  resizeHeight();
+};
+
+window.onresize = function(): void {
+  resizeHeight();
 }
