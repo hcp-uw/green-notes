7f23779b2ef556873f123a9752fb65f9c3af5d23
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2024-07-30 19:44:17 -0700

    Languages dropdown

--- starter-frontend/src/components/ide/LanguagesDropdown.tsx
+++ starter-frontend/src/components/ide/LanguagesDropdown.tsx
@@ -1,3 +1,17 @@
-export default function LanguagesDropdown(): JSX.Element {
-    return <></>;
+import React from "react";
+import Select, { SingleValue } from "react-select";
+import { languageOptions, languageOption } from "./languageOptions";
+
+type LanguagesDropdownProps = {
+    onSelectChange: (sl: SingleValue<languageOption> | null) => void
+}
+
+export default function LanguagesDropdown( { onSelectChange }: LanguagesDropdownProps ): JSX.Element {
+    return (
+        <Select 
+            options={languageOptions}
+            defaultValue={languageOptions[0]}
+            onChange={(selectedOption) => onSelectChange(selectedOption)}
+        />
+    );
 }
