1dcf98443fcc46e2a63bde2d77b564defffaefa9
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2024-08-05 15:42:47 -0700

    handleCompile function

--- starter-frontend/src/components/ide/IDE.tsx
+++ starter-frontend/src/components/ide/IDE.tsx
@@ -15,7 +15,7 @@ export default function IDE(): JSX.Element {
     const [customInput, setCustomInput] = useState<string>("");
     const [output, setOutput] = useState<boolean>(false);
     const [outputDetails, setOutputDetails] = useState<string | null>(null);
-    const [processing, setProcessing] = useState(null);
+    const [processing, setProcessing] = useState<boolean | null>(null);
     const [language, setLanguage] = useState<languageOption>(languageOptions[0]);
     
     const enterPress = useKeyPress("Enter");
@@ -49,12 +49,71 @@ export default function IDE(): JSX.Element {
     }
 
     function handleCompile(_evt: MouseEvent<HTMLButtonElement>) {
+        setProcessing(true);
+        const formData = {
+            language_id: language.id, 
+            source_code: btoa(code), 
+            stdin: btoa(customInput)
+        };
+        const options = {
+            method: "POST", 
+            url: process.env.REACT_APP_RAPID_API_URL, 
+            params: { base64_encoded: "true", fields: "*" }, 
+            headers: {
+                "content-type": "application/json", 
+                "Content-Type": "application/json",
+                "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST, 
+            }, 
+            data: formData
+        };
+
+        axios 
+            .request(options)
+            .then(function (response) {
+                console.log("res.data", response.data);
+                const token = response.data.token;
+                checkStatus(token);
+            })
+            .catch((err) => {
+                let error = err.response ? err.response.data : err;
+                setProcessing(false);
+                console.log(error);
+            });
+    };
+
+    // TO-DO: change types
+    async function checkStatus(token: string): Promise<any> {
+        const options = {
+            method: "GET", 
+            url: process.env.REACT_APP_RAPID_API_URL + "/" + token, 
+            params: { base64_encoded: "true", fields: "*" }, 
+            headers: {
+                "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST
+            }, 
+        };
+        try {
+            let response = await axios.request(options);
+            let statusId = response.data.status?.id;
+
+            if (statusId === 1 || statusId === 2) {
+                setTimeout(() => {
+                    checkStatus(token);
+                }, 2000);
+                return;
+            } else {
+                setProcessing(false);
+                setOutputDetails(response.data);
+                // TO-DO: Show success
+                console.log('response.data', response.data);
+                return;
+            }
+        } catch (err) {
+            console.log("err", err);
+            setProcessing(false);
+            // TO-DO: Show error
+        }
 
     }
-
-    // async function checkStatus(token): Promise<> {
-
-    // }
 
     function minimize(_evt: MouseEvent<HTMLButtonElement>): void {
         setOutput(false);
