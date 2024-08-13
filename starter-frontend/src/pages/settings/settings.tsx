d3935e2437f291976c2dd2373b9c70ec0236bce7
audrey157 <146869707+audrey157@users.noreply.github.com>
2024-08-12 18:47:53 -0700

    some layout changes, display real name and email

--- starter-frontend/src/pages/settings/settings.tsx
+++ starter-frontend/src/pages/settings/settings.tsx
@@ -1,5 +1,6 @@
 import './settings.css';
 import profile from '../../assets/profile-button.png';
+import { useAuth } from '../../contexts/AuthContext';
 
 type WhiteBoxProps = {type: string, body: JSX.Element};
 
@@ -15,17 +16,27 @@ function WhiteBox({type, body}: WhiteBoxProps): JSX.Element {
 // TODO: make functional/editable
 // General settings listed on the left of the page
 function GeneralSettings(): JSX.Element {
+    const user = useAuth();
+    if (user === null) {
+        throw new Error("No user found")
+    }
+
+    const currentUser = user.currentUser;
+    if (currentUser === null) {
+        throw new Error("No user found");
+    }
+
     return (
-        <div id='text'>
+        <div className='settings-text'>
             <h1>General</h1>
-            <p>Display Name: John Doe</p>
-            <p>Username: </p>
-            <p>Email: </p>
-            <p>Bio:</p>            
+            <p>Display Name: {currentUser.displayName}</p>
+            <p>Email: {currentUser.email}</p>           
             <p>Change password</p> {/*make into button/pop-up*/}
-            <p> </p>
-            <h1>Appearance</h1>
+            
+            <h1 id='appearance'>Appearance</h1>
             <p>Color Theme: <button>Default</button></p> {/*make button*/}
+
+            <button>Delete Account</button>
         </div>
     )
 }
@@ -33,9 +44,11 @@ function GeneralSettings(): JSX.Element {
 function ChangeProfilePhoto(): JSX.Element {
     return (
         <div className='profile-text'>
-            <img src={profile} id='img'></img>
-            <p>Change Photo</p>
-            <button>Delete Account</button>
+            <img src={profile} alt='profile' id='img'></img>
+            <p>Change Photo</p> {/*onclick should make a popup with options to change photo */}
+            <p></p>
+            <p id='bio'>bio text placeholder</p>
+            <p>Edit Bio</p> {/*onclick should make bio editable */}
         </div>
     )
 }
@@ -44,8 +57,8 @@ function ChangeProfilePhoto(): JSX.Element {
 export default function Settings(): JSX.Element {
     return (
         <div className='page green-background' id='main-elements'>
-            <WhiteBox type='settings' body={<GeneralSettings/>}/>
-            <WhiteBox type='profile-photo' body={<ChangeProfilePhoto />} />
+            <WhiteBox type='settings-box' body={<GeneralSettings/>}/>
+            <WhiteBox type='profile-photo-box' body={<ChangeProfilePhoto />} />
         </div>
     );
 }
