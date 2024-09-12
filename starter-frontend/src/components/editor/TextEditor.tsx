5cc00393d1848b0ff30b822e02a81b58afc68b24
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2024-09-12 16:46:44 -0700

    Added button to TinyMCE

--- starter-frontend/src/components/editor/TextEditor.tsx
+++ starter-frontend/src/components/editor/TextEditor.tsx
@@ -1,20 +1,27 @@
-import React, { useRef, useState } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import React, { useCallback, useRef, useState } from 'react';
+import { Editor, IAllProps } from '@tinymce/tinymce-react';
 import { Editor as TinyMCEEditor } from 'tinymce';
 import { auth } from '../../config/firebase';
 import { FetchRoute } from '../file-navigation/routes';
+import RegisterPlugins from './plugins/RegisterPlugins';
 
 type TextEditorProps = {
   initContent: string,
   eRoute: string,
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
-  setCurrContent:  React.Dispatch<React.SetStateAction<string>>
-}
+  setCurrContent:  React.Dispatch<React.SetStateAction<string>>, 
+  setupEditor?: (e: TinyMCEEditor) => void
+} & Partial<IAllProps>;
 
-export default function TextEditor({initContent, eRoute, setIsLoading, setCurrContent} : TextEditorProps) {
+export default function TextEditor({initContent, eRoute, setIsLoading, setCurrContent, setupEditor, init = {}, ...rest} : TextEditorProps) {
     const editorRef = useRef<TinyMCEEditor | null>(null);
 
     const [content, setContent] = useState<string>(initContent);
+
+    const setup = useCallback((editor: TinyMCEEditor) => {
+      RegisterPlugins( {editor} );
+      setupEditor && setupEditor(editor);
+    }, []);
 
     return (
       <div id="editor-area">
@@ -33,16 +40,19 @@ export default function TextEditor({initContent, eRoute, setIsLoading, setCurrCo
             plugins: [
               'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
               'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-              'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount', 'save'
+              'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount', 
+              'save'
             ],
             toolbar: 'undo redo | blocks | ' +
               'bold italic forecolor | alignleft aligncenter ' +
               'alignright alignjustify | bullist numlist outdent indent | ' +
-              'removeformat | help | save',
+              'removeformat | help | save | ide',
             content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }', 
             save_onsavecallback: (): void => {
               save(setContent, editorRef, eRoute, setIsLoading, setCurrContent)
-            }
+            }, 
+            setup, 
+            ...init,
           }}
         />
         </form>
